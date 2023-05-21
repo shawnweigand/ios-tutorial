7e894c0a98b18fe56eb02532ea9ed0f7b02b51fc
Shawn Weigand <sweigand@estee.com>
2023-05-21 11:47:45 -0400

    Initial Commit

---
+++ ios-tutorial/ios_tutorialApp.swift
@@ -0,0 +1,17 @@
+//
+//  ios_tutorialApp.swift
+//  ios-tutorial
+//
+//  Created by Weigand, Shawn on 5/21/23.
+//
+
+import SwiftUI
+
+@main
+struct ios_tutorialApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
